84a3f60a0b8cf49d2574e117ea2a19a890b01965
AndrewThian <andrewbobbythian@gmail.com>
2017-03-27 23:20:57 +0800

    josephus kata complete

--- josephus_kata.js
+++ josephus_kata.js
@@ -2,6 +2,32 @@ function test () {
   return 'Test function running'
 }
 
+function josephus (people, interval) {
+  var deaths = []
+  var index = 0
+  var len = people.length
+  // console.log('length of array', len)
+  // console.log('====== start loop ======');
+
+  while (len = people.length) {
+    // console.log('alive array length', people.length)
+    // adding % len causes the loop to break when the index is 0
+    index = (index + (interval - 1)) % len
+    // console.log('current index is', index)
+    // console.log('person', people[index], 'has died')
+    deaths.push(people[index])
+    // console.log('pushed into death', deaths)
+    // console.log('removed person', people[index], 'from alive')
+    people.splice(index, 1)
+    // console.log(people)
+    // console.log('============== event end ==============')
+  }
+  return deaths
+}
+
+josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)
+
 module.exports = {
-  test: test
+  test: test,
+  josephus: josephus
 }
--- test/josephus_test.js
+++ test/josephus_test.js
@@ -3,13 +3,35 @@
 const expect = require('chai').expect
 const kata = require('../josephus_kata.js')
 
-describe('Domain kata', function () {
+describe('Josephus kata', function () {
   describe('Init testing', function () {
     it('Test function should be running', function () {
       expect(kata.test()).to.equal('Test function running')
     })
   })
+  describe('My tests', function () {
+    it('Should return an instance of Array', function () {
+      expect(kata.josephus([1, 2, 3, 4, 5, 6, 7], 3)).to.instanceOf(Array)
+    })
+    it('Return an empty array when provided an empty array', function () {
+      expect(kata.josephus([], 3)).to.eql([])
+    })
+  })
   describe('Code wars tests', function () {
-
+    it('return permuted array of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', function () {
+      expect(kata.josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    })
+    it('return permuted array of [2, 4, 6, 8, 10, 3, 7, 1, 9, 5]', function () {
+      expect(kata.josephus([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).to.eql([2, 4, 6, 8, 10, 3, 7, 1, 9, 5])
+    })
+    it("return permuted array of ['e', 's', 'W', 'o', 'C', 'd', 'r', 'a']", function () {
+      expect(kata.josephus(['C', 'o', 'd', 'e', 'W', 'a', 'r', 's'], 4)).to.eql(['e', 's', 'W', 'o', 'C', 'd', 'r', 'a'])
+    })
+    it('return permuted array of [3, 6, 2, 7, 5, 1, 4]', function () {
+      expect(kata.josephus([1, 2, 3, 4, 5, 6, 7], 3)).to.eql([3, 6, 2, 7, 5, 1, 4])
+    })
+    it('Return an empty array', function () {
+      expect(kata.josephus([], 3)).to.eql([])
+    })
   })
 })
